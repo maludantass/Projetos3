9055ded407263d17bb9b09d946e95852f6863a9d
amanda-montarroios <@amontarroiosoliveira@gmail.com>
2025-03-23 18:13:14 -0300

    base

---
+++ brasfi-demo/src/main/java/com/brasfi/demo/BrasfiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.brasfi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrasfiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrasfiDemoApplication.class, args);
+	}
+
+}
---
+++ brasfi-demo/src/test/java/com/brasfi/demo/BrasfiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brasfi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasfiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
