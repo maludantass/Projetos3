2bdabbf07d5ef49cf1ed040c39b9275e227e8c3a
Fabiana Coelho de Souza Leão Silveira <fabicoelhodesouzaleao@gmail.com>
2025-06-02 16:00:54 -0300

    ALTERANDO forumService.js

--- react-frontend/src/services/forumService.js
+++ react-frontend/src/services/forumService.js
@@ -1,8 +1,21 @@
 import axios from 'axios';
 
-const API = 'http://localhost:8080/api/forum'; // ← PORTA CERTA!
+const API = 'http://localhost:8080/api/forum';
 
+// Buscar todas as comunidades do fórum
 export const getCommunities = async () => {
   const res = await axios.get(`${API}/communities`);
   return res.data.content;
+};
+
+// Buscar posts de uma comunidade específica
+export const getPostsByCommunity = async (communityId) => {
+  const res = await axios.get(`${API}/posts/community/${communityId}`);
+  return res.data.content;
+};
+
+//criar posts
+export const createPost = async (postData) => {
+  const res = await axios.post(`${API}/posts`, postData);
+  return res.data;
 };
