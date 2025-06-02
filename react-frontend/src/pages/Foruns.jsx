d80426be3b2767e1ea43589d77c789e8791f8930
Fabiana Coelho de Souza Leão Silveira <fabicoelhodesouzaleao@gmail.com>
2025-06-02 15:34:31 -0300

    forum front

--- react-frontend/src/pages/Foruns.jsx
+++ react-frontend/src/pages/Foruns.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react';
+import { getCommunities } from '../services/forumService';
+import { Link } from 'react-router-dom';
+
 const Foruns = () => {
-    return (
-      <div>
-        <h1>Foruns</h1>
-        <p>Conteúdo dos foruns</p>
-      </div>
-    );
-  };
-  
-  export default Foruns;
+  const [communities, setCommunities] = useState([]);
+
+  useEffect(() => {
+    getCommunities()
+      .then(setCommunities)
+      .catch((err) => console.error('Erro ao carregar comunidades:', err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Comunidades do Fórum</h1>
+      <ul>
+        {communities.map((comunidade) => (
+          <li key={comunidade.id}>
+            <Link to={`/comunidade/${comunidade.id}`}>{comunidade.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Foruns;
