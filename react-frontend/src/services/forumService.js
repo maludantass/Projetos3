0875c32c67b7f58785a89798c915ce042154aa09
Fabiana Coelho de Souza Leão Silveira <fabicoelhodesouzaleao@gmail.com>
2025-06-02 16:33:36 -0300

    TESTE forumService.js

--- react-frontend/src/services/forumService.js
+++ react-frontend/src/services/forumService.js
@@ -34,3 +34,12 @@ export const createComment = async (commentData) => {
   return res.data;
 };
 
+//TESTE
+export const createCommunity = async (communityData) => {
+  const res = await axios.post(`${API}/communities`, communityData, {
+    headers: getAuthHeader(),
+  });
+  return res.data;
+};
+
+
